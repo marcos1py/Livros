d2aadb7b13409d31d7ef6eeb62cd092ff8c81545
Marcos <marcosantoniop47@gmail.com>
2023-07-01 01:21:18 -0300

    fazendo as validaçao do

--- authors/urls.py
+++ authors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "authors"
+
 urlpatterns = [
     path('registrar/', views.registrar_view, name='registrar'),
+    path('registrar/create/', views.registrar_create, name='create'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import ReguistrarForm
+from django.http import Http404
 
 def registrar_view(request):
-    return render(request, 'authors/paginas/registrar_view.html')
+    register_form_data = request.session.get('register_form_data', None)
+    form = ReguistrarForm(register_form_data)
+
+    return render(request, 'authors/paginas/registrar_view.html', context={
+        "form": form,
+    })
+
+def registrar_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = ReguistrarForm(POST)
+
+    return redirect('authors:registrar')
