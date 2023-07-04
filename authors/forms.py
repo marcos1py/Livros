e15b2b2a5c4bb23ce3672833e72d58137d322cce
Marcos <marcosantoniop47@gmail.com>
2023-07-03 23:49:19 -0300

    melhora no form

--- authors/forms.py
+++ authors/forms.py
@@ -1,15 +1,18 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-import re
 
 
 def add_attr(field, attr_name, attr_new_val):
     existing = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
 
+
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
 
 def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
@@ -23,20 +26,46 @@ def strong_password(password):
             code='invalid'
         )
 
-class ReguistrarForm(forms.ModelForm):
 
+class RegistrarForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         add_placeholder(self.fields['username'], 'Your username')
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
+    username = forms.CharField(
+        label = 'Username',
+        help_text = (
+            "Username must have letters, number or one of those @.-+"
+            "The length should be between 4 and 150 caracter"
+        ),
+        error_messages = {
+            "required": 'this fileds must not be empty',
+            "min_length": 'Username must have at least 4 char',
+            "max_length": 'Username must have at least 150 char',
+        },
+        min_length = 4, max_length = 150,
+    )
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.',
+    )
     password = forms.CharField(
-        required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Your password'
-        }),
+        widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
         },
@@ -45,13 +74,15 @@ class ReguistrarForm(forms.ModelForm):
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
         ),
-        validators=[strong_password]
+        validators=[strong_password],
+        label='Password'
     )
     password2 = forms.CharField(
-        required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password'
-        })
+        widget=forms.PasswordInput(),
+        label='Password2',
+        error_messages={
+            'required': 'Please, repeat your password'
+        },
     )
 
     class Meta:
@@ -63,55 +94,7 @@ class ReguistrarForm(forms.ModelForm):
             'email',
             'password',
         ]
-        # exclude = ['first_name']
-        labels = {
-            'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
-            'password': 'Password',
-        }
-        help_texts = {
-            'email': 'The e-mail must be valid.',
-        }
-        error_messages = {
-            'username': {
-                'required': 'This field must not be empty',
-            }
-        }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your username here',
-                'class': 'input text-input'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password here'
-            })
-        }
-
-    def clean_password(self):
-        data = self.cleaned_data.get('password')
-
-        if 'atenção' in data:
-            raise ValidationError(
-                'Não digite %(pipoca)s no campo password',
-                code='invalid',
-                params={'pipoca': '"atenção"'}
-            )
-
-        return data
-
-    def clean_first_name(self):
-        data = self.cleaned_data.get('first_name')
-
-        if 'John Doe' in data:
-            raise ValidationError(
-                'Não digite %(value)s no campo first name',
-                code='invalid',
-                params={'value': '"John Doe"'}
-            )
-
-        return data
+        
 
     def clean(self):
         cleaned_data = super().clean()
--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from .forms import ReguistrarForm
+from .forms import RegistrarForm
 from django.http import Http404
+from django.contrib import messages
 
 def registrar_view(request):
     register_form_data = request.session.get('register_form_data', None)
@@ -17,5 +18,11 @@ def registrar_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = ReguistrarForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, "User criador com sucesso")
+        del(request.session['register_form_data'])
+        
 
     return redirect('authors:registrar')
